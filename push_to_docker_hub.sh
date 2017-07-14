e6890ee1e3ce9da3c2a02a520b590d53630ec933
Fabio José <fabiojose@gmail.com>
2017-07-14 16:50:39 -0300

    Update the dockerhub project

--- push_to_docker_hub.sh
+++ push_to_docker_hub.sh
@@ -4,10 +4,10 @@ _grafana_tag=$1
 _grafana_version=${_grafana_tag:1}
 
 if [ "$_grafana_version" != "" ]; then
-	echo "pushing grafana/grafana:${_grafana_version}"
-	docker push grafana/grafana:${_grafana_version}
-	docker push grafana/grafana:latest
+	echo "pushing fabiojose/grafana:${_grafana_version}"
+	docker push fabiojose/grafana:${_grafana_version}
+	docker push fabiojose/grafana:latest
 else
-	echo "pushing grafana/grafana:master"
-	docker push grafana/grafana:master
+	echo "pushing fabiojose/grafana:master"
+	docker push fabiojose/grafana:master
 fi
